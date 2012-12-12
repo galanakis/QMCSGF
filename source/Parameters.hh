9727262650f4f9b7ca6c275f707348db89e5ca99
Dimitris Galanakis <dimitris.galanakis@gmail.com>
2012-12-12 20:24:59 +0800

    I fixed an error in the previous fix.

--- source/Parameters.hh
+++ source/Parameters.hh
@@ -441,10 +441,10 @@ struct Parameters {
       // at least one of "MeasTime" and "WarmIterations" should exist
       assert(d["MeasTime"].IsInt() || d["MeasIterations"].IsInt());
 
-      WarmTime=d["WarmTime"].IsInt() ? d["WarmTime"].GetInt64() : -1;
-      WarmIterations=d["WarmIterations"].IsInt() ? d["WarmIterations"].GetInt64() : -1 ;
-      MeasTime=d["MeasTime"].IsInt() ? d["MeasTime"].GetInt64() : -1;
-      MeasIterations=d["MeasIterations"].IsInt() ? d["MeasIterations"].GetInt64() : -1;
+      WarmTime=d["WarmTime"].IsInt64() ? d["WarmTime"].GetInt64() : -1;
+      WarmIterations=d["WarmIterations"].IsInt64() ? d["WarmIterations"].GetInt64() : -1 ;
+      MeasTime=d["MeasTime"].IsInt64() ? d["MeasTime"].GetInt64() : -1;
+      MeasIterations=d["MeasIterations"].IsInt64() ? d["MeasIterations"].GetInt64() : -1;
 
       // Names of the operators to measure
       const rapidjson::Value &measure=d["Measure"];
